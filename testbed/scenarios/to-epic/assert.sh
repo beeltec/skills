aa1d34f764a83302a23d5e2acf5026a130140e30
Christian Beelte <cb@beelte.com>
2026-07-27 14:03:53 +0200

    fix(to-epic): link epic directories from index

--- testbed/scenarios/to-epic/assert.sh
+++ testbed/scenarios/to-epic/assert.sh
@@ -5,6 +5,8 @@ set -u
 assert_glob 'docs/backlog/epics/EPIC-*'
 assert_grep_glob 'type: epic' 'docs/backlog/epics/*/EPIC-*.md'
 assert_grep_glob 'status: ready' 'docs/backlog/epics/*/EPIC-*.md'
+assert_grep '^- \[EPIC-[0-9]{3,}: [^]]+\]\(EPIC-[^)]*/\) - .+' docs/backlog/epics/index.md
+assert_not_grep '\]\(EPIC-[^)]*/EPIC-[0-9]{3,}\.md\)' docs/backlog/epics/index.md
 # At least two executable children inside the Epic directory, each parented to it.
 assert_glob 'docs/backlog/epics/*/WORK-*.md'
 assert_grep_glob 'parent: EPIC-' 'docs/backlog/epics/*/WORK-*.md'
